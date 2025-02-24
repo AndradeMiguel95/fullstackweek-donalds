3c0d8d228ab4306561175974fc0c8f35e0b42f36
Miguel Andrade <k2.miguel@grupofleury.com.br>
2025-02-23 23:25:41 -0300

    feat: ensure cache is revalidated when creating an order
    Signed-off-by: Miguel Andrade <k2.miguel@grupofleury.com.br>

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/prisma";
 
@@ -56,5 +57,6 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+  revalidatePath(`/${input.slug}/ordes`);
   redirect(`/${input.slug}/orders`);
 };
